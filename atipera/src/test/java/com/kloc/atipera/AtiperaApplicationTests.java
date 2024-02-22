b7d559c2c7297c435942caebc1c5f7e63d5971c5
Bartlomiej Kloc <bartlomiejkloc00@gmail.com>
2024-02-22 12:04:46 +0100

    init commit

---
+++ atipera/src/test/java/com/kloc/atipera/AtiperaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kloc.atipera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiperaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
